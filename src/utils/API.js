48eb1d7d41b5eef716bd891325194d6f58383b52
Nate Griggs <nateghsc09@gmail.com>
2021-08-23 20:47:11 -0500

    fixed starting files

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
+const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+
+// Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-  // Gets all users
-  getUsers: function() {
-    return axios.get("https://randomuser.me/api/?results=200&nat=us");
+  search: function(query) {
+    return axios.get(BASEURL + query + APIKEY);
   }
 };
