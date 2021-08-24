27bbecf9c8f88d9a64afa5567ee97642ea019f67
Nate Griggs <nateghsc09@gmail.com>
2021-08-23 20:55:05 -0500

    build

---
+++ build/precache-manifest.ffc54e8fb5ce755d25ecc6fa13f5f6b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78968cfd1cc00f564f4c3304512fa0d8",
+    "url": "/nateghsc09/react-directory/index.html"
+  },
+  {
+    "revision": "2854c642b7430b6b5313",
+    "url": "/nateghsc09/react-directory/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "38d18d256f24b65231e6",
+    "url": "/nateghsc09/react-directory/static/css/main.5d1a1003.chunk.css"
+  },
+  {
+    "revision": "2854c642b7430b6b5313",
+    "url": "/nateghsc09/react-directory/static/js/2.d8e493c0.chunk.js"
+  },
+  {
+    "revision": "38d18d256f24b65231e6",
+    "url": "/nateghsc09/react-directory/static/js/main.4e41bb3a.chunk.js"
+  },
+  {
+    "revision": "fc1b85f3b9b4f03590a6",
+    "url": "/nateghsc09/react-directory/static/js/runtime~main.3aba9cf3.js"
+  }
+]);
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
-const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+const BASEURL = "https://randomuser.me/api/?results=50";
 
-// Export an object with a "search" method that searches the Giphy API for the passed query
+
 export default {
-  search: function(query) {
-    return axios.get(BASEURL + query + APIKEY);
+  fetchEmployees: function() {
+    return axios.get(BASEURL);
   }
 };
