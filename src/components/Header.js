1f5204f67db3e4c2f972fad290b5cf7bcc3eb58b
Nate Griggs <nateghsc09@gmail.com>
2021-08-26 20:57:25 -0500

    rearange everything

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
-import "../styles/Header.css";
 
-export default class Header extends Component {
-  render() {
+
+import React from "react";
+const Header = (props) => {
     return (
-      <div className="header">
-        <h1>Employee Directory</h1>
-        <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
-      </div>
-    )
-  }
+<div className="header">React Employee Directory
+</div>
+);
 }
+export default Header;
